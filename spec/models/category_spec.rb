1f7bc417b58b7b56797eb98846d5edc2cac1681b
dylankb <dylan.barnard@gmail.com>
2018-02-13 17:06:51 -0800

    Refactor tests to test our own code
    Previous tests were testing Rails library code, which isn't ideal

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,24 +1,5 @@
 require 'spec_helper'
 
 describe Category do
-  it 'can save' do
-    category = Category.new(name: 'Comedy')
-    category.save
-    expect(Category.first).to eq(category)
-  end
-
-  it 'has many videos' do
-    commedy = Category.create(name: 'Comedy')
-    the_office = Video.create(title: 'The Office', category: commedy)
-    family_guy = Video.create(title: 'Family Guy', category: commedy)
-    expect(commedy.videos).to include(the_office, family_guy)
-  end
-
-  it 'has videos ordered by title' do
-    commedy = Category.create(name: 'Comedy')
-    the_office = Video.create(title: 'The Office', category: commedy)
-    family_guy = Video.create(title: 'Family Guy', category: commedy)
-    expect(commedy.videos).not_to eq([the_office, family_guy])
-    expect(commedy.videos).to eq([family_guy, the_office])
-  end
+  it { should have_many(:videos) }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,15 +1,7 @@
 require 'spec_helper'
 
 describe Video do
-  it 'should be able to save' do
-    video = Video.new(title: 'New video')
-    video.save
-    Video.first.title.should == video.title
-  end
-
-  it 'belongs to one category' do
-    scifi = Category.new(name: 'Science Fiction & Fantasy')
-    got = Video.new(title: 'Game of Thrones', category: scifi)
-    expect(got.category).to eq(scifi)
-  end
+  it { should belong_to(:category) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
 end
