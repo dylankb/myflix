28ed1d5df5b1708908fac02fcb6d23617d3e8cec
dylankb <dylan.barnard@gmail.com>
2018-02-14 18:57:41 -0800

    Allow a user to register
    - Create users table
    - Add pages controller for static pages
    - Add register and sign in pages
    - Add password encryption library

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
-  root to: redirect('/home')
-  get '/home', to: 'videos#index', as: 'home'
+  root to: 'pages#front'
 
   resources :videos, only: [:show] do
     collection do
-      get :search, to: "videos#search"
+      get :search, to: 'videos#search'
     end
   end
+
+  get 'register', to: 'users#new'
+  get 'sign_in', to: 'sessions#new'
+  resources :users, only: [:create]
 
   # These also work
 
