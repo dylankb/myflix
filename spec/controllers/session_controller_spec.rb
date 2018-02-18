3276202926420dde61747b868c7fac0503b3232c
dylankb <dylan.barnard@gmail.com>
2018-02-18 14:13:02 -0800

    Test app authentication
    Write tests for session and user controllers

---
+++ spec/controllers/session_controller_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+describe SessionsController do
+  describe 'GET new' do
+    it 'renders the new template for unauthenticated users' do
+      get :new
+      expect(response).to render_template :new
+    end
+    it 'redirects to the home path for authenticated users' do
+      session[:user_id] = Fabricate(:user).id
+      get :new
+      expect(response).to redirect_to home_path
+    end
+  end
+
+  describe 'POST create' do
+    let(:bob) { Fabricate(:user) }
+
+    context 'with valid credentials' do
+      before do
+        post :create, email: bob.email, password: bob.password
+      end
+      it 'puts the signed in user in the session' do
+        expect(session[:user_id]).to eq(bob.id)
+      end
+      it 'should redirect to the home path' do
+        expect(response).to redirect_to home_path
+      end
+      it 'sets the notice' do
+        expect(flash[:notice]).not_to be_nil
+      end
+    end
+
+    context 'with valid credentials' do
+      before do
+        post :create, email: bob.email, password: ''
+      end
+      it 'does not put the signed in user in the session' do
+        expect(session[:user_id]).to be_blank
+      end
+      it 'redirects to the sign in page' do
+        expect(response).to redirect_to sign_in_path
+      end
+      it 'sets the error message' do
+        expect(flash[:error]).not_to be_blank
+      end
+    end
+  end
+
+  describe 'GET destroy' do
+    before do
+      session[:user_id] = Fabricate(:user).id
+      get :destroy
+    end
+    it 'clears the session for the user' do
+      expect(session[:user_id]).to be_nil
+    end
+    it 'redirects to the root path' do
+      expect(response).to redirect_to root_path
+    end
+    it 'sets the notice message' do
+      expect(flash[:notice]).not_to be_blank
+    end
+  end
+end
