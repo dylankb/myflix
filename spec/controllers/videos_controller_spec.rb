ca58b8ba8a4fae7150671f027dc24d5feec7f618
dylankb <dylan.barnard@gmail.com>
2018-02-17 17:03:58 -0800

    Add video and category controller tests
    Added fabrication and faker gems for mocking data

---
+++ spec/controllers/videos_controller_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+describe VideosController do
+  describe 'GET index' do
+    it 'sets @categories for authenticated user' do
+      session[:user_id] = Fabricate(:user).id
+      category = Fabricate(:category)
+      get :index
+
+      expect(assigns(:categories)).to eq([category])
+    end
+  end
+
+  describe 'GET show' do
+    context 'with authenticated user' do
+      before do
+        session[:user_id] = Fabricate(:user).id
+      end
+
+      it 'sets @video' do
+        video = Fabricate(:video)
+        get :show, id: video.id
+        expect(assigns(:video)).to eq(video)
+      end
+    end
+
+    context 'with unauthenticated user' do
+      it 'redirects user to the sign in page' do
+        video = Fabricate(:video)
+        get :show, id: video.id
+        expect(response).to redirect_to sign_in_path
+      end
+    end
+  end
+
+  describe 'POST search' do
+    it 'rests @results for authenticated users' do
+      session[:user_id] = Fabricate(:user).id # "flatter" test with no context
+      the_office = Fabricate(:video, title: 'The Office')
+      get :search, search_term: 'The'
+      expect(assigns(:results)).to eq([the_office])
+    end
+    it 'redirects to sign in page for the unauthenticated users' do
+      the_office = Fabricate(:video, title: 'The Office')
+      get :search, search_term: 'The'
+      expect(response).to redirect_to sign_in_path
+    end
+  end
+end
