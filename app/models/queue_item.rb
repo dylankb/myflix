482ffd788694c2663fa17bafc879d8162a0fa6f3
dylankb <dylan.barnard@gmail.com>
2018-07-21 11:14:33 -0700

    Refactor queue view to obey LoD
    Reaching through less objects is better (Law of Demeter)

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,4 +1,10 @@
 class QueueItem < ActiveRecord::Base
   belongs_to :video
   belongs_to :user
+
+  delegate :average_rating, to: :video, prefix: true
+  delegate :title, to: :video, prefix: true
+
+  delegate :category, to: :video
+  delegate :name, to: :category, prefix: :video_category
 end
--- spec/models/queue_item_spec.rb
+++ spec/models/queue_item_spec.rb
@@ -3,4 +3,36 @@ require 'spec_helper'
 describe QueueItem do
   it { should belong_to(:user) }
   it { should belong_to(:video) }
+
+  describe "#video_title" do
+    it "returns the title of the video" do
+      video = Fabricate(:video)
+      queue_item = Fabricate(:queue_item)
+      queue_item.video = video
+
+      expect(queue_item.video_title).to eq(video.title)
+    end
+  end
+
+  describe "#video_average_rating" do
+    it "returns the rating of the video" do
+      video = Fabricate(:video)
+      queue_item = Fabricate(:queue_item)
+      queue_item.video = video
+
+      expect(queue_item.video_average_rating).to eq(video.average_rating)
+    end
+  end
+
+  describe "#video_category" do
+    it "returns the category of the video" do
+      video = Fabricate(:video)
+      category = Fabricate(:category, name: "Comedy")
+      video.category = category
+      queue_item = Fabricate(:queue_item)
+      queue_item.video = video
+
+      expect(queue_item.video_category_name).to eq("Comedy")
+    end
+  end
 end
